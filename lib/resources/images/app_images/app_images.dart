9e84e3725e63f13d06ae814a0f3c81eddbbe1b07
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-13 13:04:05 +0300

    remove unusable image asset

--- lib/resources/images/app_images/app_images.dart
+++ lib/resources/images/app_images/app_images.dart
@@ -1,5 +1,4 @@
 class AppImages {
   static const _baseImagePath = 'assets/images/app_images';
   static const icErrorOutline = '$_baseImagePath/ic_error_outline.png';
-  static const icPlaceholder = '$_baseImagePath/ic_placeholder.png';
 }
