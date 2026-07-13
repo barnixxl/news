47de85e5391dd49531d8c26f1b59cbca001299c5
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-13 14:03:10 +0300

    remove unusable color

--- lib/resources/colors/app_colors.dart
+++ lib/resources/colors/app_colors.dart
@@ -6,6 +6,5 @@ class AppColors {
   static const primaryDark = Color(0xFF1565C0);
   static const onPrimary = Color(0xFFFFFFFF);
   static const error = Color(0xFFF44336);
-  static const shadow = Color(0xFF1976D2);
   static const primaryGray = Color(0xFF777777);
 }
