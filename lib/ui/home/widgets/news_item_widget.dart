adf47d9cf0b80eefa8cd96ec8fe5fea6629ef117
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-13 15:16:58 +0300

    made button on widget

--- lib/ui/home/widgets/news_item_widget.dart
+++ lib/ui/home/widgets/news_item_widget.dart
@@ -4,21 +4,24 @@ import '../../../main.dart';
 import '../../../models/news_item.dart';
 import '../../../resources/colors/app_colors.dart';
 import '../../../resources/dimens/app_dimens.dart';
+import '../../../resources/images/app_images/app_images.dart';
 import '../../../utils/date_formatter.dart';
 import '../../common/app_card/app_card_widget.dart';
 
 class NewsItemWidget extends StatelessWidget {
   final NewsItem newsItem;
+  final void Function(String link)? onLinkPressed;
 
   const NewsItemWidget({
     super.key,
     required this.newsItem,
+    this.onLinkPressed,
   });
 
   @override
   Widget build(
-      BuildContext context,
-      ) {
+    BuildContext context,
+  ) {
     return AppCardWidget(
       clipContent: true,
       child: Padding(
@@ -45,6 +48,24 @@ class NewsItemWidget extends StatelessWidget {
                 style: const TextStyle(
                   color: AppColors.primaryGray,
                   fontSize: AppDimens.fontXs,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+                child: TextButton.icon(
+                  onPressed: () => onLinkPressed?.call(newsItem.link),
+                  icon: Image.asset(
+                    AppImages.icOpenInNew,
+                    width: AppDimens.iconXs,
+                    height: AppDimens.iconXs,
+                    color: AppColors.primary,
+                  ),
+                label: Text(
+                  strings.read_more,
                 ),
               ),
             ),
