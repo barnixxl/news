49e92bda174972e6a4afb8c0fa57fa6d7abfbde4
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-13 15:16:12 +0300

    add path to link button

--- lib/resources/images/app_images/app_images.dart
+++ lib/resources/images/app_images/app_images.dart
@@ -1,4 +1,6 @@
 class AppImages {
   static const _baseImagePath = 'assets/images/app_images';
+
   static const icErrorOutline = '$_baseImagePath/ic_error_outline.png';
+  static const icOpenInNew = '$_baseImagePath/ic_open_in_new.png';
 }
